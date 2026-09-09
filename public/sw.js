d251dd0f58a5a1860cf564e0531019d2a3a1ddd2
shamzx <shamzx@users.noreply.github.com>
2026-09-09 23:40:16 +0530

    Fix shell reload loop (reload once per version); bump shell v19 / SW v10

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'assembleonline-shell-v9'
+const CACHE = 'assembleonline-shell-v10'
 const STATIC_ASSETS = ['/', '/index.html', '/manifest.webmanifest', '/logo.jpeg']
 
 self.addEventListener('install', (event) => {
