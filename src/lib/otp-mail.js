f888b4426e147f60dfaef258c423049c3cb04e40
Meiyarasan2005 <meiyarasanaarumani@gmail.com>
2026-09-09 23:21:16 +0530

    Fix EmailJS template params: email/passcode/time to match One-Time Password template

--- src/lib/otp-mail.js
+++ src/lib/otp-mail.js
@@ -5,5 +5,14 @@ const TEMPLATE_ID = 'template_qi8q2ko'
 const PUBLIC_KEY = 'WEQpxxQ9KIsa_RbgW'
 
 export async function sendOtpEmail({ to_email, otp }) {
-  return emailjs.send(SERVICE_ID, TEMPLATE_ID, { to_email, otp }, { publicKey: PUBLIC_KEY })
+  /* Template params must match the EmailJS "One-Time Password" template:
+     To Email = {{email}}, code = {{passcode}}, expiry = {{time}}. */
+  const expiresAt = new Date(Date.now() + 10 * 60 * 1000)
+  const time = expiresAt.toLocaleTimeString('en-IN', { hour: 'numeric', minute: '2-digit' })
+  return emailjs.send(
+    SERVICE_ID,
+    TEMPLATE_ID,
+    { email: to_email, passcode: otp, time },
+    { publicKey: PUBLIC_KEY },
+  )
 }
