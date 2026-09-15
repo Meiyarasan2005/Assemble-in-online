4db5ca7a9b52aa32e6f37389af5284ba4f810aa3
Meiyarasan2005 <meiyarasanaarumani@gmail.com>
2026-09-15 22:42:31 +0530

    Remove Orders button from header & mobile menu; show filters by default on desktop
    - Header: remove the Orders button next to Wishlist and the Orders/Track links from the mobile menu (IconBox import dropped)
    - Shop: filter sidebar now open by default on desktop so items are easy to identify; still a full-screen drawer on mobile

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'assembleonline-shell-v18'
+const CACHE = 'assembleonline-shell-v19'
 const STATIC_ASSETS = ['/', '/index.html', '/manifest.webmanifest', '/logo.jpeg']
 
 self.addEventListener('install', (event) => {
