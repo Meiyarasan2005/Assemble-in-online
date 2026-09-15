992bd450e02a2d417ab7b39c0c9b33089ea4b7e7
shamzx <shamzx@users.noreply.github.com>
2026-09-15 17:07:58 +0530

    Remove green rating badge and review count from product cards

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'assembleonline-shell-v14'
+const CACHE = 'assembleonline-shell-v15'
 const STATIC_ASSETS = ['/', '/index.html', '/manifest.webmanifest', '/logo.jpeg']
 
 self.addEventListener('install', (event) => {
